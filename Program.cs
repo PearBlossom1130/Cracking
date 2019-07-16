c53bd9bedcbc7557db40d76d86fbfd173cac6a93
Hwasoo Lee <Hwasoo.Lee@versanthealth.com>
2019-07-16 14:17:29 -0400

    [MASTER] Add IsSubString()

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace chap1
         static void Main(string[] args)
         {
             var b = CheckPermutation2("Welcome","dsghjkt");
+            var c = Urlify("   Hello World    ");
+
+            var d = IsSubstring("WaterBottle", "terBottleWa");
         }
 
         static bool IsUnique(string s) {
@@ -54,6 +57,69 @@ namespace chap1
             return true;
         }
 
+        static string Urlify(string s) {
+            var trimmedString = s.Trim();
+            int count = 0;
+            foreach(var c in trimmedString) {
+                if(c == 32) count++;
+            }
 
+            var dd = new char[trimmedString.Length+count*2];
+            int j = 0;
+            for(var i = 0; i < trimmedString.Length; i++) {
+                if(trimmedString[i] == 32) {
+                    dd[j++] = '%';
+                    dd[j++] = '2';
+                    dd[j++] = '0';
+                }
+                else {
+                    dd[j++] = trimmedString[i];
+                }
+            }
+    
+            return new string(dd);
+        }
+
+        static bool IsPermutationOfPalindrome(string phase) {
+            // int[] table = BuildCharFrequencyTable(phase);
+
+            // return CheckMaxOneOdd(table);
+            return true;
+        }
+
+        static bool CheckMaxOneOdd(int[] table) {
+            bool foundOdd = false;
+            for(int i =0; i < table.Length; i++) {
+                if(i % 2 == 1) {
+                    if(foundOdd) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            return true;
+        }
+
+        static int GetCharNumber(char c) {
+            int a = 'a';
+            int z = 'z';
+
+            int val =  c;
+
+            if (a <= val && val <= z) {
+                return val - a;
+            }
+
+            return -1;
+        }
+
+        static bool IsSubstring(string s1, string s2)
+        {
+            if(s1.Length != s2.Length) return false;
+
+            var s1s1 = s1+s1;
+            return s1s1.Contains(s2) ? true : false;
+        }
     }
 }
